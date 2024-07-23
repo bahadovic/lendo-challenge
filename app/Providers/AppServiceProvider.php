f776cc619806f55538cdbc07f1219156cc4e71d8
bahadovic <b.hosseini@arzeshwallet.com>
2024-07-23 20:13:32 +0330

    fix AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(SmsDriverFactory::class, function ($app) {
-            return new SmsDriverFactory();
-        });
+
     }
 
     /**
